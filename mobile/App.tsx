951a575c3e00da1aa72c4408a51364f21af46757
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-01-19 20:00:34 -0300

    feat: adiciona tela home na aplicação mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,9 @@
-import { StyleSheet, StatusBar, Text, View } from 'react-native';
+import './src/lib/dayjs'
+
+import { StatusBar} from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from "@expo-google-fonts/inter"
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,23 +20,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar barStyle="light-content"  backgroundColor="transparent" translucent/>
-    </View>
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
