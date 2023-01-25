022a4e1c50e5d35590a2b38ad17de7fa70725e50
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-01-25 18:26:19 -0300

    feat: utilizando dados da API no Popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,14 +3,19 @@ import * as Checkbox from '@radix-ui/react-checkbox'
 import clsx from 'clsx'
 import { ProgressBar } from './ProgressBar';
 import { Check } from 'phosphor-react';
+import dayjs from 'dayjs';
 
 interface HabitDayProps {
-    completed: number
-    amount: number
+    date: Date
+    completed?: number
+    amount?: number
 }
 
-export function HabitDay({ amount, completed }: HabitDayProps) {
-    const completedPercentage = Math.round((completed / amount) * 100)
+export function HabitDay({ amount = 0, completed = 0, date }: HabitDayProps) {
+    const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
+
+    const dayAndMonth = dayjs(date).format('DD/MM')
+    const dayOfWeek = dayjs(date).format('dddd')
 
     return (
         <Popover.Root>
@@ -19,15 +24,15 @@ export function HabitDay({ amount, completed }: HabitDayProps) {
                 "bg-violet-600 border-violet-500": completedPercentage >= 60 && completedPercentage < 80,
                 "bg-violet-700 border-violet-500": completedPercentage >= 40 && completedPercentage < 60,
                 "bg-violet-800 border-violet-600": completedPercentage >= 20 && completedPercentage < 40,
-                "bg-violet-900 border-violet-700": completedPercentage >= 0 && completedPercentage < 20,
+                "bg-violet-900 border-violet-700": completedPercentage > 0 && completedPercentage < 20,
                 "bg-zinc-900 border-zinc-800": completedPercentage === 0,
 
             })} />
             <Popover.Portal>
                 <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-                    <span className="font-semibold text-zinc-400">domingo</span>
-                    <span className="mt-1 font-extrabold leading-tight text-3xl">22/01</span>
-                    <ProgressBar progress={100} />
+                    <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
+                    <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
+                    <ProgressBar progress={completedPercentage} />
 
                     <div className="mt-6 flex flex-col gap-3">
                         <Checkbox.Root
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,13 +1,32 @@
+import dayjs from "dayjs"
+import { useEffect, useState } from "react"
+import { api } from "../lib/axios"
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDatesFromYearBeginning()
+
 const minimumSummaryDatesSize = 18 * 7
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
+type Summary = {
+    id: string;
+    date: string;
+    amount: number;
+    completed: number;
+}[]
+
 export function SummaryTable() {
+    const [summary, setSummary] = useState<Summary>([])
+
+    useEffect(() => {
+        api.get('/summary').then(response => {
+            setSummary(response.data)
+        })
+    }, [])
+
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -24,8 +43,17 @@ export function SummaryTable() {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day')
+                    })
+
                     return (
-                        <HabitDay amount={5} completed={Math.round(Math.random() * 5)} key={date.toString()} />
+                        <HabitDay
+                            date={date}
+                            amount={dayInSummary?.amount}
+                            completed={dayInSummary?.completed}
+                            key={date.toString()}
+                        />
                     )
                 })}
 
